05fa4e5839dfe323b10eb10e02ec0ef228be7d5c
WarerCode <warercode@gmail.com>
2024-09-03 23:50:08 +0400

    Create MainWindow.h

---
+++ C++ SFML/Classes/MainWindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include<SFML/Graphics.hpp>
+#include<SFML/Window.hpp>
+
+#include<map>
+
+typedef unsigned int uint, id;
+typedef std::pair<id, sf::RectangleShape*> toDo;
+
+class MainWindow : public sf::RenderWindow
+{
+private:
+	static sf::RenderWindow* pWin;
+
+	static uint width;
+	static uint height;
+	static uint notec;
+
+	static sf::VideoMode* videoMode;
+	static const sf::String title;
+	static const sf::Color fill;
+
+	static std::map<id, sf::RectangleShape*> notes;
+
+public:
+	MainWindow();
+	virtual ~MainWindow();
+
+	// Construct
+	static void initWindow();
+	static void initVariables();
+
+	static void pollEvents();
+	static void render();
+	static void addNote(toDo* thing);
+};
+
+#endif
