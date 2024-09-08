1f689e837d19fa0d0f3714a4025bf642988d37ac
WarerCode <warercode@gmail.com>
2024-09-09 00:05:13 +0400

    Update PushButton.cpp

--- C++ SFML/Classes/PushButton.cpp
+++ C++ SFML/Classes/PushButton.cpp
@@ -1,5 +1,7 @@
 #include "PushButton.h"
 
+const float PushButton::outlineThickness = 2;
+const color PushButton::line = { 16, 16, 16 };
 const color PushButton::on = {0, 255, 0};
 const color PushButton::off = {255, 0, 0};
 
@@ -8,6 +10,9 @@ PushButton::PushButton(int x, int y, int xPos, int yPos) {
 	this->triggered = new bool{ false }; // strange maybe?
 	this->buttonRect = new rect{ static_cast<sf::Vector2f>(*this->size) };
 	this->buttonRect->setPosition(static_cast<sf::Vector2f>(vec2i{ xPos, yPos }));
+	this->buttonRect->setOutlineThickness(outlineThickness);
+	this->buttonRect->setOutlineColor(line);
+
 	if (*(this->triggered)) {
 		this->buttonRect->setFillColor(on);
 	}
