bb743f0eb393e8835a3db92e356902c2d606165b
WarerCode <warercode@gmail.com>
2024-09-15 23:28:57 +0400

    Add files via upload

---
+++ C++ SFML/Classes/General.h
@@ -0,0 +1,13 @@
+#ifndef GENERAL_H
+#define GENERAL_H
+
+#define WINDOW_WIDTH 800.f
+#define WINDOW_HEIGHT 1000.f
+#define BACK_FILL sf::Color{255, 255, 100}
+#define EPSILONE 0.7f			// it width of note in windows
+#define DELTA 10				// count of notes in window
+
+#include<iostream>
+#include<fstream>
+
+#endif
